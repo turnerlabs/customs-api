416f4007b96887cde4ded757e20566c9fdbe4dd6
Wilson Wise <wilson.wise@turner.com>
2016-09-01 10:44:55 -0400

    add tests for catalog endpoint

--- test/index.js
+++ test/index.js
@@ -171,3 +171,108 @@ describe('Deploy', function () {
             });
     });
 });
+
+describe('Catalog', function () {
+    let testAuthToken = 'test-token',
+        shipit = 'http://shipit.test.services.dmtio.net',
+        catalogit = 'http://catalogit.test.services.dmtio.net',
+        path = '/catalog/my-shipment/test-env/fake-barge',
+        data = {
+            name: "my-container",
+            image: "registry.example.com/my-container:0.1.0",
+            version: "0.1.0"
+        };
+
+    beforeEach(function () {
+        nock(shipit)
+            .get('/v1/shipment/my-shipment/environment/test-env')
+            .replyWithFile(200, getMockData('shipit'));
+
+        nock(catalogit)
+            .post('/v1/containers', {
+                name: "my-container",
+                image: "registry.example.com/my-container:0.1.0",
+                version: "0.0.0"
+            })
+            .replyWithFile(409, getMockData('catalogit-conflict'))
+
+        nock(catalogit)
+            .post('/v1/containers', data)
+            .replyWithFile(200, getMockData('catalogit'));
+    });
+
+    it('should 400 when a required field is missing', function (done) {
+        request(server)
+            .post(path)
+            .set('x-build-token', testAuthToken)
+            .send({image: "something"})
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    done(err);
+                }
+
+                let body = res.body;
+
+                expect(body).to.deep.equal({
+                    message: 'missing field name, missing field version'
+                });
+
+                done();
+            });
+    });
+
+    it('should 401 when no token provided', function (done) {
+        request(server)
+            .post(path)
+            .send(data)
+            .expect(401, done);
+    });
+
+    it('should 403 when wrong token provided', function (done) {
+        let testAuthToken = 'wrong-token';
+
+        request(server)
+            .post(path)
+            .set('x-build-token', testAuthToken)
+            .send(data)
+            .expect(403, done);
+    });
+
+    it('should fail with 409 if container already exists', function (done) {
+        request(server)
+            .post(path)
+            .set('x-build-token', testAuthToken)
+            .send({
+                catalog: true,
+                name: "my-container",
+                image: "registry.example.com/my-container:0.1.0",
+                version: "0.0.0"
+            })
+            .expect(409, done);
+    });
+
+    it('should succeed', function (done) {
+        request(server)
+            .post(path)
+            .set('x-build-token', testAuthToken)
+            .send(data)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                let body = res.body;
+
+                expect(body).to.deep.equal({
+                    message: [
+                        'authenticated and authorized',
+                        'cataloged my-container v0.1.0'
+                    ].join(', ')
+                });
+
+                done();
+            });
+    });
+});
